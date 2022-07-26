38320cc24370a4cf4d01e1e4959aee53c0e849ac
franciscovinicios <francisco.viniciosti@gmail.com>
2022-07-26 16:20:04 -0300

    Page Success - State Global React Hook Form

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -4,9 +4,29 @@ import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
 import { ContainerText, IconContainer, InfoItem, OrderDetails, OrderInfo, SuccessContainer } from "./styles";
 
 import MotoboyImg from '../../assets/motoboy.svg'
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { OrderData } from "../ Checkout";
+import { paymentMethods } from "../ Checkout/components/CompleteOrder/PaymentMethods";
+
+
+
+interface LocationType {
+  state: OrderData;
+}
 
 
 export function Success() {
+
+  const navigate = useNavigate()
+  const { state } = useLocation() as unknown as LocationType;
+  useEffect(() => {
+    if (!state) {
+      navigate("/");
+    }
+  }, []);
+
+  if (!state) return <></>;
 
   return (
     <SuccessContainer>
@@ -25,8 +45,8 @@ export function Success() {
             <IconContainer className="map-pin" >
               <MapPin size={14} weight="fill" />
             </IconContainer>
-            <span>Entrega em <span className="bold">Rua João Daniel Martinelli, 102</span> <br />
-              Farrapos - Porto Alegre,  RS,
+            <span>Entrega em <span className="bold">{state.street}, {state.number}</span> <br />
+              {state.district}- {state.city},{state.uf}
             </span>
           </InfoItem>
 
@@ -44,7 +64,7 @@ export function Success() {
               <CurrencyDollar size={14} weight="fill" />
             </IconContainer>
             <span>Pagamento na entrega<br />
-              <span className="bold">Cartão de Crédito</span>
+            <span className="bold">{paymentMethods[state.paymentMethod].label}</span>
             </span>
           </InfoItem>
         </OrderInfo>
